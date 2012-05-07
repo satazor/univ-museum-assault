9d5a389a09b346554c8c969bd517a4e126665e04
Andre Cruz <andremiguelcruz@msn.com>
2012-05-07 09:58:57 +0100

    Fix ips.

--- museumassault/common/Configuration.java
+++ museumassault/common/Configuration.java
@@ -30,9 +30,9 @@ public class Configuration implements IThievesConfiguration
     protected ArrayList<Integer> corridorIds;
     protected ArrayList<Integer> teamIds;
 
-    protected String sharedSiteThievesConnectionString = "localhost:11000";  // The server address must be equal to the line bellow
-    protected String sharedSiteChiefsConnectionString = "localhost:11001";   // The server address must be equal to the line above
-    protected String loggerConnectionString = "localhost:15000";
+    protected String sharedSiteThievesConnectionString = "192.168.8.171:20000";  // The server address must be equal to the line bellow
+    protected String sharedSiteChiefsConnectionString = "192.168.8.171:20001";   // The server address must be equal to the line above
+    protected String loggerConnectionString = "192.168.8.180:20000";
 
     protected HashMap<Integer, String> roomConnections;
     protected HashMap<Integer, String> corridorConnections;
@@ -43,8 +43,6 @@ public class Configuration implements IThievesConfiguration
      */
     public Configuration()
     {
-        this.roomConnections = new HashMap<Integer, String>();
-
         this.roomIds = new ArrayList(this.nrRooms);
         for (int x = 0; x < this.nrRooms; x++) {
             this.roomIds.add(x + 1);
@@ -75,18 +73,19 @@ public class Configuration implements IThievesConfiguration
             this.teamIds.add(x + 1);
         }
 
-        this.roomConnections.put(1, "localhost:20000");
-        this.roomConnections.put(2, "localhost:20001");
-        this.roomConnections.put(3, "localhost:20002");
-        this.roomConnections.put(4, "localhost:20003");
-        this.roomConnections.put(5, "localhost:20004");
+        this.roomConnections = new HashMap<Integer, String>();
+        this.roomConnections.put(1, "192.168.8.175:20000");
+        this.roomConnections.put(2, "192.168.8.175:20001");
+        this.roomConnections.put(3, "192.168.8.176:20000");
+        this.roomConnections.put(4, "192.168.8.177:20000");
+        this.roomConnections.put(5, "192.168.8.178:20000");
 
         this.corridorConnections = new HashMap<Integer, String>();
-        this.corridorConnections.put(1, "localhost:21000");
-        this.corridorConnections.put(2, "localhost:21001");
-        this.corridorConnections.put(3, "localhost:21002");
-        this.corridorConnections.put(4, "localhost:21003");
-        this.corridorConnections.put(5, "localhost:21004");
+        this.corridorConnections.put(1, "192.168.8.175:20002");
+        this.corridorConnections.put(2, "192.168.8.175:20003");
+        this.corridorConnections.put(3, "192.168.8.176:20001");
+        this.corridorConnections.put(4, "192.168.8.177:20001");
+        this.corridorConnections.put(5, "192.168.8.178:20001");
 
         this.roomCorridorAssociations = new HashMap<Integer, Integer>();
         this.roomCorridorAssociations.put(1, 1);
